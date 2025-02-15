b9694e8a6fc7a2c565dbe00420f6af73adb8dddf
utshobbose <utshob.bose@g.bracu.ac.bd>
2025-02-16 00:55:32 +0600

    Hero Section Updated

--- app/components/Home/Home.tsx
+++ app/components/Home/Home.tsx
@@ -1,8 +1,11 @@
 import React from 'react'
+import Hero from './Home/Hero/Hero';
 
 const Home = () => {
     return (
-        <div className='overflow-hidden h-[10000px]'></div>
+        <div className='overflow-hidden h-[10000px]'>
+            <Hero/>
+        </div>
     )
     }
 
