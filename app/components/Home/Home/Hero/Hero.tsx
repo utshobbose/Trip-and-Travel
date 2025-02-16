661f395e2687752ff3d9a5ee8858217a433fdaf5
utshobbose <utshob.bose@g.bracu.ac.bd>
2025-02-16 14:11:56 +0600

    Update Hero section

--- app/components/Home/Home/Hero/Hero.tsx
+++ app/components/Home/Home/Hero/Hero.tsx
@@ -2,8 +2,19 @@ import React from 'react'
 
 const Hero = () => {
 return (
-    <div>
-    Hero
+    <div className='relative w-full h-[100vh] sm:h-[100vh]'>
+        {/* Overlay */}
+        <div className='absolute top-0 left-0 w-full h-full bg-gray-400 opacity-60'>
+        <video 
+        src="/images/Mountain View Stock Footage .mp4"
+        autoPlay
+        loop
+        muted
+        preload='metadata'
+        className='w-full h-full object-cover'
+        ></video>
+        </div>
+        
     </div>
 )
 }
